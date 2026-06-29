e72a49e383310e9aaa1234aabd43b5053961ba32
Damilola <whakhydoh@gmail.com>
2026-06-29 14:32:14 +0100

    profile

--- src/pages/admin.tsx/VerificationPage.tsx
+++ src/pages/admin.tsx/VerificationPage.tsx
@@ -112,7 +112,7 @@ export default function VerificationPage() {
 
       {/* TABLE */}
 
-      <div className="bg-white rounded-2xl shadow overflow-hidden">
+      <div className="bg-white rounded-2xl shadow overflow-x-auto">
 
         <table className="w-full">
 
